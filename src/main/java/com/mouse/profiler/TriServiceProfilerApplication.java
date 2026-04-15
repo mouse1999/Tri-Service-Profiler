24601518cc040547cd65ca6568b4f4cfe6200f25
mouse1999 <kufreedward26@gmail.com>
2026-04-15 13:30:49 +0100

    feat:initialized project repo with dependencies

---
+++ src/main/java/com/mouse/profiler/TriServiceProfilerApplication.java
@@ -0,0 +1,13 @@
+package com.mouse.profiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriServiceProfilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriServiceProfilerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mouse/profiler/TriServiceProfilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouse.profiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriServiceProfilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
